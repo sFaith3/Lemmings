22aa9954ca8c662263b9b0b65a969b37e533846a
Viamos <victor.amoros@balmes.epiaedu.cat>
2016-05-21 16:25:15 +0200

    canvios Lemming Añadidos los estados para las distintas animaciones

--- Lemmings/Lemmings/Lemming.h
+++ Lemmings/Lemmings/Lemming.h
@@ -17,6 +17,7 @@ private:
 	int velocitat;
 	bool paraigues;
 	bool escalar;
+	int pintW, pintH;
 
 public:
 	Lemming();
@@ -37,28 +38,32 @@ public:
 	/*
 	MOVE: Moures, FALL: Caure, BREAK: Trencar Parets, GLIDE: Levitar, DIG: Cavar,
 	STOP: Inmovilitzat, STAIR: Posar rajoles,PICK: Cavar amb el Pic 
-	EXPLOSION: Explota als 5s, DEAD: El Lemming es destruit, DEADFALL: El Lemming cau d'una gran altura
-	OPENUMBRELLA: El Lemming obra el paraigües, NOSTAIR: El Lemming es queda sense rajoles
-	*/
+	EXPLOSION: Foc de l'Explosio s, DEAD: El Lemming es destruit, DEADFALL: El Lemming cau d'una gran altura
+	OPENUMBRELLA: El Lemming obra el paraigües, NOSTAIR: El Lemming es queda sense rajoles, ENDCLIMB El Lemming acaba d'escalar i torna a terra pla
+	EXPLODING: El Lemming fa l'animacio de que va a explotar*/
 	enum EstatsEnum{
 		MOVE, FALL, BREAK, GLIDE, CLIMB, DIG, PICK, STOP, STAIR, EXPLOSION, 
-		DEAD, DEADFALL, OPENUMBRELLA, NOSTAIR
+		DEAD, DEADFALL, OPENUMBRELLA, NOSTAIR, ENDCLIMB, EXPLODING
 	};
 	int GetEstat();
 
 	// Sets per canviar l'Estat 
-	void SetMoure();
-	void SetCaure();
+	void SetMoure();   
+	void SetCaure();   
 
-	void SetTrencar();
-	void SetPrincipiLevitar();
-	void SetLevitar();
-	void SetCavar();
-	void SetPicar();
-	void SetInmovilitzar();
-	void SetConstruirEscala();
+	void SetForadarParet(); 
+	void SetObrirParaigues();
+	void SetPlanejarParaigues(); 
+	void SetCavar(); 
+	void SetPicar(); 
+	void SetInmovilitzar(); 
+	void SetConstruirEscala(); 
 	void SetNoEscales();
+	void SetExplosio(); 
 	void SetExplotar();
+	void SetEscalar(); 
+	void SetFinalEscalar(); 
+	void SetMortCaure(); 
 
 	// Estats temporals 
 	void Moure();
